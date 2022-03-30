ab6fe9895609cba47221f5f107b6f657a1f21487
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2022-03-30 19:41:46 +0100

    Add argparser and some basic input checks

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from argparse import ArgumentParser
 from bokeh.models import ColumnDataSource
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, output_file, save
@@ -8,14 +9,16 @@ PLOT_WIDTH = 1200
 BACKGROUND_COLOUR = "#fafafa"
 
 
-def main():
+def main(args):
     # Data from https://www.geog.ox.ac.uk/research/climate/rms/daily-data.html
     weather_data_raw = pd.read_csv("daily-data-to-dec-2020.csv")
 
     # handle non-numeric instances like where data is missing
     weather_data_raw = weather_data_raw.apply(pd.to_numeric, errors="coerce")
 
-    weather_data_subset = weather_data_raw[weather_data_raw["YYYY"] >= 2019]
+    weather_data_subset = weather_data_raw[((weather_data_raw["YYYY"] >= args.start_year) & (
+        weather_data_raw["YYYY"] <= args.start_year + (args.num_years-1)))]
+
     weather_data = weather_data_subset.reset_index(drop=True)
 
     # Make a datetime column to use as x-index
@@ -29,12 +32,13 @@ def main():
 
     print("Running...")
     # set output to static HTML file
-    output_file(filename="/tmp/output.html", title="Weather data")
+    output_file(filename=f"{args.output_dir}/weather_plots.html",
+                title="Weather data")
     source = ColumnDataSource(weather_data)
 
     # create plots
     s1 = figure(background_fill_color=BACKGROUND_COLOUR,
-                sizing_mode="stretch_width", width=PLOT_WIDTH, x_axis_type='datetime')
+                sizing_mode="stretch_width", width=PLOT_WIDTH, x_axis_type="datetime")
     s1.quad(top="Tmax °C", bottom="Tmin °C", left="left", right="right",
             source=source, color=palette[0], alpha=0.2, legend_label="min/max")
 
@@ -48,4 +52,22 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument("--output_dir", type=str, default="",
+                        help="Path to folder where outputs will be saved")
+    parser.add_argument("--start_year", type=int, default=2019,
+                        help="Year from which to start analysis")
+    parser.add_argument("--num_years", type=int, default=1,
+                        help="Number of years to analyse")
+    args = parser.parse_args()
+
+    if args.start_year < 1815 or args.start_year > 2020:
+        raise ValueError(
+            f"Invalid start year: {args.start_year} - this must be between 1815 and 2020.")
+
+    if args.start_year + args.num_years > 2020:
+        raise ValueError(
+            f"The requested start year ({args.start_year}) plus the entered "
+            f"number of years ({args.num_years}) exceeds the latest available data from the end of 2020.")
+
+    main(args)
